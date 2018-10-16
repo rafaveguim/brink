5a65fbff4509ee7b7da3628dbc8e8882714702ed
Rafael <rafaveguim@gmail.com>
2018-10-15 21:59:16 -0400

    [feat] Adjust filter_size params and add HSV option

--- mssim.py
+++ mssim.py
@@ -24,23 +24,19 @@ warnings.filterwarnings('ignore')
 import numpy as np
 from scipy import signal
 from scipy.ndimage.filters import convolve
-import tensorflow as tf
 from skimage.transform import resize
 from pathlib import Path
 
+import imageio
 import itertools
 import argparse
-
+import cv2
 from tqdm import tqdm
 
 from multiprocessing.pool import Pool
 
 from math import factorial
-
-tf.flags.DEFINE_string('original_image', None, 'Path to PNG image.')
-tf.flags.DEFINE_string('compared_image', None, 'Path to PNG image.')
-FLAGS = tf.flags.FLAGS
-
+import math
 
 image_cache = dict() # image path -> array[0:M], where M is the number of scales
 
@@ -60,15 +56,15 @@ def _FSpecialGauss(size, sigma):
 
 
 def _SSIMForMultiScale(img1, img2, max_val=255, filter_size=11,
-                       filter_sigma=1.5, k1=0.01, k2=0.03):
+                       filter_sigma=1.5, k1=0.01, k2=0.03, debug=False):
   """Return the Structural Similarity Map between `img1` and `img2`.
 
   This function attempts to match the functionality of ssim_index_new.m by
   Zhou Wang: http://www.cns.nyu.edu/~lcv/ssim/msssim.zip
 
   Arguments:
-    img1: Numpy array holding the first RGB image batch.
-    img2: Numpy array holding the second RGB image batch.
+    img1, img2: Numpy array holding an RGB image batch of shape (1, width, height, 3)
+          or a grayscale image of shape (1, width, height, 1).
     max_val: the dynamic range of the images (i.e., the difference between the
       maximum the and minimum allowed values).
     filter_size: Size of blur kernel to use (will be reduced for small images).
@@ -80,8 +76,8 @@ def _SSIMForMultiScale(img1, img2, max_val=255, filter_size=11,
       the original paper).
 
   Returns:
-    Pair containing the mean SSIM and contrast sensitivity between `img1` and
-    `img2`.
+    Pair containing the mean SSIM, and the product of the constrast and structure
+     components of the SSIM calculation.
 
   Raises:
     RuntimeError: If input images don't have the same shape or don't have four
@@ -96,10 +92,19 @@ def _SSIMForMultiScale(img1, img2, max_val=255, filter_size=11,
 
   img1 = img1.astype(np.float64)
   img2 = img2.astype(np.float64)
+
+  if debug:
+    import matplotlib.pyplot as plt
+    plt.imshow(img1[0, :, :, 0], cmap='gray')
+    plt.waitforbuttonpress()
+    plt.imshow(img2[0, :, :, 0], cmap='gray')
+    plt.waitforbuttonpress()
+
   _, height, width, _ = img1.shape
 
   # Filter size can't be larger than height or width of images.
   size = min(filter_size, height, width)
+  size = max(filter_size, 1)
 
   # Scale down sigma if a smaller filter size is used.
   sigma = size * filter_sigma / filter_size if filter_size else 0
@@ -108,7 +113,7 @@ def _SSIMForMultiScale(img1, img2, max_val=255, filter_size=11,
   # see 1) http://matlabtricks.com/post-19/calculating-standard-deviation-using-minimal-memory
   #     2) http://matlabtricks.com/post-20/calculate-standard-deviation-case-of-sliding-window
 
-  if filter_size:
+  if filter_size > 1:
     window = np.reshape(_FSpecialGauss(size, sigma), (1, size, size, 1))
     mu1 = signal.fftconvolve(img1, window, mode='valid')
     mu2 = signal.fftconvolve(img2, window, mode='valid')
@@ -134,14 +139,20 @@ def _SSIMForMultiScale(img1, img2, max_val=255, filter_size=11,
   c2 = (k2 * max_val) ** 2
   v1 = 2.0 * sigma12 + c2
   v2 = sigma11 + sigma22 + c2
-  ssim = np.mean((((2.0 * mu12 + c1) * v1) / ((mu11 + mu22 + c1) * v2)))
-  cs = np.mean(v1 / v2)
+  ssim_map = ((2.0 * mu12 + c1) * v1) / ((mu11 + mu22 + c1) * v2)
+  ssim = np.mean(ssim_map)
+  cs_map = v1 / v2
+  cs = np.mean(cs_map)
+
+  if debug:
+      plt.imshow(cs_map[0, :, :, 0])
+      plt.waitforbuttonpress()
 
   return ssim, cs
 
 
-def MultiScaleSSIM(img1_list, img2_list, max_val=255, filter_size=11, filter_sigma=1.5,
-                   k1=0.01, k2=0.03, weights=None, components=False):
+def MultiScaleSSIM(img1_list, img2_list, max_val=255, filter_size=11,
+    filter_sigma=1.5, k1=0.01, k2=0.03, weights=None, components=False, debug=False):
   """Return the MS-SSIM score between `img1` and `img2`.
 
   This function implements Multi-Scale Structural Similarity (MS-SSIM) Image
@@ -153,8 +164,9 @@ def MultiScaleSSIM(img1_list, img2_list, max_val=255, filter_size=11, filter_sig
   http://www.cns.nyu.edu/~lcv/ssim/msssim.zip
 
   Arguments:
-    img1: Numpy array holding the first RGB image batch.
-    img2: Numpy array holding the second RGB image batch.
+    img1_list, img2_list: list holding M images, where M is the number of scales.
+        Each element is a Numpy array holding an RGB image batch of shape
+        (1, width, height, 3) or a grayscale image of shape (1, width, height, 1).
     max_val: the dynamic range of the images (i.e., the difference between the
       maximum the and minimum allowed values).
     filter_size: Size of blur kernel to use (will be reduced for small images).
@@ -180,12 +192,17 @@ def MultiScaleSSIM(img1_list, img2_list, max_val=255, filter_size=11, filter_sig
   weights = np.array(weights if weights else
                      [0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
 
+  if isinstance(filter_size, int):
+    filter_size = [filter_size]*len(weights)
+  if len(filter_size) == 1:
+    filter_size = filter_size*len(weights)
+
   mssim = np.array([])
-  mcs = np.array([])
-  for im1, im2 in zip(img1_list, img2_list):
+  mcs = np.array([]) #
+  for i, (im1, im2) in enumerate(zip(img1_list, img2_list)):
     ssim, cs = _SSIMForMultiScale(
-        im1, im2, max_val=max_val, filter_size=filter_size,
-        filter_sigma=filter_sigma, k1=k1, k2=k2)
+        im1, im2, max_val=max_val, filter_size=filter_size[i],
+        filter_sigma=filter_sigma, k1=k1, k2=k2, debug=debug)
     mssim = np.append(mssim, ssim)
     mcs = np.append(mcs, cs)
 
@@ -200,7 +217,7 @@ def MultiScaleSSIM(img1_list, img2_list, max_val=255, filter_size=11, filter_sig
 class MSSIM_Caller():
   def __init__(self, **kwargs):
     self.kwargs = kwargs
-    
+
   def call(self, in_tuple):
     image1_name, image2_name = in_tuple
     img1 = image_cache[image1_name]
@@ -212,9 +229,12 @@ def options():
     parser = argparse.ArgumentParser()
     parser.add_argument('images', nargs='+')
     parser.add_argument('--num_processes', default=2)
-    parser.add_argument('--weights', nargs='+', 
+    parser.add_argument('--weights', nargs='+',
     	type=float, default=[0.0448, 0.2856, 0.3001, 0.2363, 0.1333])
     parser.add_argument('--components', action='store_true')
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--filter_size', nargs='+',
+        type=int, default=[11, 9, 7, 5, 3])
 
     return parser.parse_args()
 
@@ -222,35 +242,43 @@ def options():
 def main():
   opts = options()
 
+  debug = opts.debug
   num_processes = opts.num_processes
   image_paths = opts.images
+  filter_size = opts.filter_size
 
   images = []
   image_names = [Path(p).stem for p in image_paths]
-  
+
   for img_path in image_paths:
-    with tf.gfile.FastGFile(img_path, mode='rb') as image_file:
-      img_str = image_file.read()
+    HSV = False
 
-    input_img = tf.placeholder(tf.string)
-    decoded_image = tf.expand_dims(tf.image.decode_png(input_img, channels=3), 0)
+    if HSV:
+      img = imageio.imread(img_path, format="PNG-PIL")
+      img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+      img = np.array([img])
+      img = img.astype(np.float64)
+      # interpolate hue to 255 (in cv2, hue lies in [0,179])
+      img[:, :, :, 0] *= 255/179
+    else:
+      img = imageio.imread(img_path, format="PNG-PIL", pilmode="L")
+      img = np.array([img])
+      img = np.expand_dims(img, axis=3)
 
-    with tf.Session() as sess:
-      img = sess.run(decoded_image, feed_dict={input_img: img_str})
-    
-    images.append(img.astype(np.float64))
+    print(img.shape)
+    images.append(img)
 
   # Scale all images to lowest size
   width  = min(map(lambda x: x.shape[2], images))
   height = min(map(lambda x: x.shape[1], images))
 
-  for i, img in enumerate(images):	
-    img = resize(img, (1, height, width, 3))
-
+  for i, img in enumerate(images):
+    # img = resize(img, (1, height, width, 1))
+    # print(img)
 	# downsample img
     downsample_filter = np.ones((1, 2, 2, 1)) / 4.0
     image_cache[image_names[i]] = [img]
-	
+
     for _ in range(len(opts.weights)-1):
       x = convolve(img, downsample_filter, mode='reflect')
       img = x[:, ::2, ::2, :]
@@ -260,7 +288,7 @@ def main():
   n_combinations = factorial(n_images) / (factorial(2) * factorial(n_images-2) )
   pairs = itertools.combinations(image_names, 2)
   pool = Pool(num_processes)
-  caller = MSSIM_Caller(weights=opts.weights, components=opts.components)
+  caller = MSSIM_Caller(weights=opts.weights, components=opts.components, debug=debug, filter_size=filter_size)
   results = []
 
 
@@ -271,7 +299,7 @@ def main():
 
   for result in results:
     if opts.components:
-      
+
       img1_name, img2_name, (mcs, mssim) = result
       print("{},{},{},{}".format(
 	    img1_name,
@@ -279,7 +307,7 @@ def main():
 	    ','.join(map(str,mcs)),
 	    ','.join(map(str,mssim))))
     else:
-      
+
       img1_name, img2_name, mssim = result
       print("{},{},{:f}".format(
 	    img1_name,
